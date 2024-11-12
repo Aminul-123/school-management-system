959bdcc03d5d6129880ce4330449d823837d8e42
Aminul Ali <alomamin963@gmail.com>
2024-11-12 14:00:37 +0530

    clg-project

---
+++ script.js
@@ -0,0 +1,177 @@
+
+import { class1, class2 , class3, class4 } from "./students.js"
+//element selectors
+const form = document.querySelector('form')
+
+const formHtml = `<div>
+                <label for="name">Name</label>
+                <input type="text" id="name" placeholder="Type your name" required >
+            </div>
+            <div>
+                <label for="id-num">ID</label>
+                <input type="text" id="id-num"  placeholder="Type ID number" required>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Type your password" required>
+            </div>
+            
+            <button class="btn-login btn">Login</button>`
+
+form.insertAdjacentHTML('afterbegin', formHtml)
+
+const input_name = document.getElementById('name')
+const input_ID = document.getElementById('id-num')
+// console.log(inputName);
+const input_password = document.getElementById('password');
+const loginBtn = document.querySelector('.btn-login');
+const loginForm = document.querySelector('.login-form');
+const imgSlide = document.querySelector('.img-slide');
+const studentList = document.querySelector('.student-list')
+//btns
+const btn1 = document.querySelector('.btn-1')
+const btn2 = document.querySelector('.btn-2')
+const btn3 = document.querySelector('.btn-3')
+const btn4 = document.querySelector('.btn-4')
+
+const _class1 = document.querySelector('.class-1')
+const _class2 = document.querySelector('.class-2')
+const _class3 = document.querySelector('.class-3')
+const _class4 = document.querySelector('.class-4')
+
+const loginState = {
+    name : '',
+    id : '',
+    password: ''
+}
+
+
+input_name.addEventListener('change', (e) => {
+        input_name.value = e.target.value
+        loginState.name = input_name.value
+})
+input_ID.addEventListener('change', (e) => {
+    input_ID.value = e.target.value
+    loginState.id = input_ID.value
+})
+input_password.addEventListener('change', (e) => {
+    input_password.value = e.target.value;
+    
+    loginState.password = input_password.value
+})
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(loginState);
+
+    if (loginState.name === 'Alex' || loginState.id === '222' || loginState.password=== '222' ) {
+        alert('Login Successful')
+        loginForm.classList.add('hide') 
+    } else {
+        alert('Login Failed !');
+    } 
+})
+// setTimeout(function () {
+//     alert('By Default our application uses a fake authentication, so use name : alex , id: 123 and password : 222')
+// }, 5000)
+
+//img slider
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
+const img1 = 'img/_20772283-189c-4510-b1af-b645151d2742.jpg'
+const img2 = "img/_6561e201-c527-45e1-a7a3-1e479d1e3353.jpg"
+const img3 = "img/_77355d3a-1a15-4254-8ed2-b9ef4428e1fc.jpg"
+const img4 = "img/_d928eee7-399a-4c22-916a-8b3b4e09c24c.jpg"
+const img5 = "img/_e6e00022-cb22-40a4-b350-a136ef5cf88d.jpg"
+const img6 = "img/_eac93dbc-45ab-4707-a93a-eea5f77d076f.jpg"
+
+const imgArr = [img1, img2, img3, img4, img5, img6]
+let counter = 1
+
+imgArr.map((elem) => {
+
+    let imgHtml = `<img src=${elem} alt="img" class="sliding-img">`;
+    // console.log(imgHtml)
+    imgSlide.insertAdjacentHTML('afterbegin', imgHtml);
+})
+
+const slides = document.querySelectorAll('.sliding-img')
+// console.log(slides);
+
+slides.forEach((elem, idx) => {
+    elem.style.left = `${idx * 25}rem`;
+})
+
+const slideImgFunc = function () {
+    slides.forEach((elem) => {
+        elem.style.transform = `translateX(-${counter * 25}rem)`
+    })
+}
+// let displayImg = imgArr[0] ;
+next.addEventListener('click', () => {
+    slideImgFunc()
+    if(counter < 5) {
+        counter = counter + 1;
+    } else {
+        counter = 0
+    }
+})
+prev.addEventListener('click', () => {
+    slideImgFunc()
+    if (counter >= 0) {
+        counter = counter - 1
+    } else {
+        counter = 5;
+    }
+})
+
+
+
+// STUDENT INFO. TABS
+const getStudentList = function (students, class_name ) {
+    students.reverse().forEach((student) => {
+        const singleStudentHtml =  ` <div class='student-data'>
+        <span>${student.id}</span>
+        <span>${student.name}</span>
+        <span>${student.roll_no}</span>
+       </div>`
+      // console.log(singleStudentHtml);
+       class_name.insertAdjacentHTML('afterbegin', singleStudentHtml)
+    })
+}
+getStudentList(class1, _class1)
+getStudentList(class4, _class4)
+getStudentList(class3 , _class3)
+getStudentList(class2, _class2)
+
+const setZ_IndexToZero = function (first, sec, third, firstBtn, secBtn, thirdBtn) {
+    first.style.zIndex = 0
+    sec.style.zIndex = 0
+    third.style.zIndex = 0
+    firstBtn.style.backgroundColor = 'rgb(129, 129, 250)'
+    secBtn.style.backgroundColor = 'rgb(129, 129, 250)'
+    thirdBtn.style.backgroundColor = 'rgb(129, 129, 250)'
+}
+
+const btnClicked = function (b1, c1, c2, c3, c4, b2, b3, b4) {
+    // b1 -> btn to apply
+    //c1 -> class to  apply with respect to its btn
+
+    b1.addEventListener('click', () => {
+        c1.style.zIndex = 100;
+        b1.style.backgroundColor = 'orange'
+        //all other btns and classes to pass in the func below
+        setZ_IndexToZero(c2, c3, c4, b2, b3, b4)
+
+    })
+}
+
+//for btn 1
+btnClicked(btn1, _class1, _class2, _class3, _class4, btn2, btn3, btn4 )
+//for btn 2
+//order in which args is passed  matters
+btnClicked(btn2, _class2, _class1, _class3, _class4, btn1, btn3, btn4 )
+//for btn 3
+btnClicked(btn3, _class3, _class1, _class2, _class4, btn1, btn2, btn4 )
+//for btn 4
+btnClicked(btn4, _class4,  _class1, _class2, _class3,  btn1, btn3, btn2)
---
+++ students.js
@@ -0,0 +1,63 @@
+
+export const class1 = [
+  { id: 1, name: "Alice Johnson", roll_no: "A001" },
+  { id: 2, name: "Bob Smith", roll_no: "A002" },
+  { id: 3, name: "Charlie Brown", roll_no: "A003" },
+  { id: 4, name: "Diana Evans", roll_no: "A004" },
+  { id: 5, name: "Evan Davis", roll_no: "A005" },
+  { id: 6, name: "Fiona Scott", roll_no: "A006" },
+  { id: 7, name: "George Wilson", roll_no: "A007" },
+  { id: 8, name: "Hannah Lee", roll_no: "A008" },
+  { id: 9, name: "Ian Thompson", roll_no: "A009" },
+  { id: 10, name: "Julia Turner", roll_no: "A010" },
+]
+ 
+export const class2 = [
+    { id: 11, name: "Kevin Young", roll_no: "A011" },
+    { id: 12, name: "Laura King", roll_no: "A012" },
+    { id: 13, name: "Mark Green", roll_no: "A013" },
+    { id: 14, name: "Nina Baker", roll_no: "A014" },
+    { id: 15, name: "Oscar Hall", roll_no: "A015" },
+    { id: 16, name: "Pauline Carter", roll_no: "A016" },
+    { id: 17, name: "Quincy Phillips", roll_no: "A017" },
+    { id: 18, name: "Rachel Collins", roll_no: "A018" },
+    { id: 19, name: "Sam Rogers", roll_no: "A019" },
+    { id: 20, name: "Tina Powell", roll_no: "A020" },
+]
+export const class3 = [
+
+    { id: 21, name: "Umar Murphy", roll_no: "A021" },
+    { id: 22, name: "Vicky Cox", roll_no: "A022" },
+    { id: 23, name: "Walter Peterson", roll_no: "A023" },
+    { id: 24, name: "Xena Ward", roll_no: "A024" },
+    { id: 25, name: "Yasmine Rivera", roll_no: "A025" },
+    { id: 26, name: "Zack Richardson", roll_no: "A026" },
+    { id: 27, name: "Amber Butler", roll_no: "A027" },
+    { id: 28, name: "Blake Henderson", roll_no: "A028" },
+    { id: 29, name: "Cara Reed", roll_no: "A029" },
+    { id: 30, name: "Derek Ortiz", roll_no: "A030" },
+]
+export const class4 = [
+
+    { id: 31, name: "Ella Morales", roll_no: "A031" },
+    { id: 32, name: "Finn Bell", roll_no: "A032" },
+    { id: 33, name: "Gina Kelly", roll_no: "A033" },
+    { id: 34, name: "Hugo Price", roll_no: "A034" },
+    { id: 35, name: "Isabel Scott", roll_no: "A035" },
+    { id: 36, name: "Jack Gray", roll_no: "A036" },
+    { id: 37, name: "Kara Adams", roll_no: "A037" },
+    { id: 38, name: "Leo Perry", roll_no: "A038" },
+    { id: 39, name: "Mia Hughes", roll_no: "A039" },
+    { id: 40, name: "Nolan Fisher", roll_no: "A040" },
+]
+//   { id: 41, name: "Olivia Graham", roll_no: "A041" },
+//   { id: 42, name: "Pete Nelson", roll_no: "A042" },
+//   { id: 43, name: "Quinn Torres", roll_no: "A043" },
+//   { id: 44, name: "Ruby Flores", roll_no: "A044" },
+//   { id: 45, name: "Sean Price", roll_no: "A045" },
+//   { id: 46, name: "Tara Reed", roll_no: "A046" },
+//   { id: 47, name: "Uma Barnes", roll_no: "A047" },
+//   { id: 48, name: "Vince Watson", roll_no: "A048" },
+//   { id: 49, name: "Wendy Ross", roll_no: "A049" },
+//   { id: 50, name: "Xavier Foster", roll_no: "A050" }
+// ];
